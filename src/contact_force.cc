3851d54fe55c95a303114c1b67a3a63f2bf4a87a
gleb <kgill-leebr@ukr.net>
2016-02-23 17:00:26 +0200

    new physics is on

--- src/contact_force.cc
+++ src/contact_force.cc
@@ -32,12 +32,36 @@
 using namespace Coordinates;
 using namespace Geometry;
 
+// ==================== local functions` declarations =======================
+
+void _test_springs( ContactDetector::Contact& c, Globals& siku );
+
 // -----------------------------------------------------------------------
+
+void _hopkins_frankenstein( ContactDetector::Contact& c, Globals& siku );
+
+// ============================== contact push ==============================
 
 void contact_push( ContactDetector::Contact& c, Globals& siku )
 {
+  switch( siku.cont_force_model )
+  {
+    case CF_TEST_SPRINGS: //same as CF_DEFAULT
+      _test_springs( c, siku );
+      break;
+
+    case CF_HOPKINS:
+      _hopkins_frankenstein( c, siku );
+      break;
+  }
+}
+
+// ============================== definitions ==============================
+
+void _test_springs( ContactDetector::Contact& c, Globals& siku )
+{
   int ires;  // !static? temporal variable to store geometry results
-  vec3d tv; // !static
+  vec3d tv; // !static?
 
   // TODO: such errors should be removed by removing their reason
   if( (siku.es[c.i1].flag & Element::F_ERRORED) ||
@@ -45,9 +69,9 @@ void contact_push( ContactDetector::Contact& c, Globals& siku )
     return;
 
   mat3d src_to_dest = loc_to_loc_mat( siku.es[c.i1].q, siku.es[c.i2].q );
-      // !static
+      // !static?
   mat3d dest_to_src = loc_to_loc_mat( siku.es[c.i2].q, siku.es[c.i1].q );
-      // !static
+      // !static?
 
   std::vector<vec2d> loc_P1;  // e1.P vertices in local 2d coords
   std::vector<vec2d> loc_P2;  // e2.P vertices in local 2d coords
@@ -277,4 +301,193 @@ void contact_push( ContactDetector::Contact& c, Globals& siku )
       c.durability -= (t > epsilon) ? t * sigma : 0.;
     }
 
+}
+
+// -----------------------------------------------------------------------
+
+void _hopkins_frankenstein( ContactDetector::Contact& c, Globals& siku )
+{
+  int ires;  // !static? temporal variable to store geometry results
+  vec3d tv; // !static?
+
+  // TODO: such errors should be removed by removing their reason
+  if( (siku.es[c.i1].flag & Element::F_ERRORED) ||
+      (siku.es[c.i2].flag & Element::F_ERRORED) )
+    return;
+
+  mat3d src_to_dest = loc_to_loc_mat( siku.es[c.i1].q, siku.es[c.i2].q );
+      // !static?
+  mat3d dest_to_src = loc_to_loc_mat( siku.es[c.i2].q, siku.es[c.i1].q );
+      // !static?
+
+  std::vector<vec2d> loc_P1;  // e1.P vertices in local 2d coords
+  std::vector<vec2d> loc_P2;  // e2.P vertices in local 2d coords
+  for( auto& p : siku.es[c.i1].P )
+    loc_P1.push_back( vec3_to_vec2( p ) );
+  for( auto& p : siku.es[c.i2].P )
+    loc_P2.push_back( vec3_to_vec2( src_to_dest * p ) );
+
+  if( errored( loc_P1 ) )
+    {
+      siku.es[c.i1].flag |= Element::F_ERRORED;
+    }
+  if( errored( loc_P2 ) )
+    {
+      siku.es[c.i2].flag |= Element::F_ERRORED;
+    }
+
+  ///*static*/ point2d center( 0, 0 ); // !static
+  vec2d center;
+  double area;
+  vector<PointStatus> stats;
+  vector<vec2d> interPoly;
+
+  if( c.type != ContType::JOINT )
+    {
+
+      // calculating intersection zone and properties
+      if( intersect( loc_P1, loc_P2, interPoly, &stats, &center, &area ) > 2 )
+        {
+          // mark contact as a 'collision'
+          c.type = ContType::COLLISION;
+
+
+
+          //TODO: clean this mess
+          vec2d p1p2 [2];  // p1p2 = two points: p1 and p2
+          size_t np = 0;  // amount of edge-edge intersections
+
+          // noob search for p1 and p2
+          for(size_t i = 0; i < stats.size(); ++i )
+            if( stats[i] == PointStatus::EDGE )
+              {
+                if( np < 2 )  p1p2[ np ] = interPoly[ i ];
+                np++;
+              }
+
+          if( np != 2 )  // inapplicable  //UNDONE: add solution for 'fencing'
+                                          // intersections
+            {
+              c.type = ContType::NONE;
+              return ;
+            }
+
+          c.generation = 0;  // refreshing contact for avoiding deletion
+
+////////////////////////
+          // point from e1 center to e2 center
+          tv = src_to_dest * NORTH;
+          vec2d r12 { tv.x, tv.y };
+
+          // and its ort
+          vec2d ort { r12 / sqrt( tv.x*tv.x + tv.y*tv.y ) };
+
+          // point from e2 center to aim
+          vec2d r2 { center - r12 };
+
+          // e1 aim speed (coz of spin + propagation)
+          vec2d v1 { vec3_to_vec2( siku.es[c.i1].V ) +
+              rot_90_cw( center ) * ( - siku.es[c.i1].W.z  * siku.planet.R ) };
+
+          // e2 aim speed (spin + propagation)
+          vec2d v2 { vec3_to_vec2( src_to_dest * siku.es[c.i2].V )
+                     + rot_90_cw( center - r12 ) *
+                     ( - siku.es[c.i2].W.z * siku.planet.R ) };
+
+          // velocity difference at aim point
+          vec2d v12 { v1 - v2 };
+////////////////////////
+
+          // directions and applying point
+          vec2d dp = p1p2[1] - p1p2[0];
+          vec2d tau = dp.ort() * copysign( 1., cross( p1p2[0], dp ) );
+          vec2d norm { tau.y, -tau.x };
+          vec2d center = ( p1p2[0] + p1p2[1] ) / 2.;
+
+          double Kne = siku.phys_consts[0],
+                 Kni = siku.phys_consts[1],
+                 Kw = siku.phys_consts[2],
+                 Kt = siku.phys_consts[3];
+
+          double Asqrt = sqrt( area );
+
+          double da_dt = siku.time.get_dt() ?
+              ( Asqrt - sqrt( c.area ) ) / siku.time.get_dt() : 0.;
+
+          vec2d F = ( Kne * Asqrt  +
+                      Kni * da_dt ) * siku.planet.R * norm;
+
+          //////////  testing tangential force
+          F += tau * ( tau * v12 ) * Asqrt * Kt * siku.planet.R2;
+
+          double torque1 =
+              Kw * siku.planet.R_rec * cross( center, F );
+
+          double torque2 =
+              Kw * siku.planet.R_rec * cross( center -
+              vec3_to_vec2( src_to_dest * NORTH ), F );
+
+          // signs are fitted manually
+          siku.es[c.i1].F -= vec2_to_vec3( F );
+          siku.es[c.i1].N -= torque1;
+
+          siku.es[c.i2].F += dest_to_src * vec2_to_vec3( F );
+          siku.es[c.i2].N += torque2;
+
+          c.area = area;
+
+        }
+    }
+  else  // <=> if( c.type == ContType::JOINT ) // Hopkins` physics
+    {
+      double K = siku.phys_consts[5];
+      double Kw = siku.phys_consts[6];
+      double sigma = siku.phys_consts[7];
+      double epsilon = siku.phys_consts[8];
+
+      vec2d p11 = vec3_to_vec2( siku.es[c.i1].P[c.v11] );
+      vec2d p12 = vec3_to_vec2( siku.es[c.i1].P[c.v12] );
+      vec2d p21 = vec3_to_vec2( src_to_dest * siku.es[c.i2].P[c.v21] );
+      vec2d p22 = vec3_to_vec2( src_to_dest * siku.es[c.i2].P[c.v22] );
+      vec2d X;
+
+      double sinX;
+      double cosX;
+
+      if( line_inter( p11, p12, p21, p22, X ) ) /// or line_seg_inter
+        {
+          sinX = cross( (p12-p11).ort(), ( p21-p22 ).ort() );
+          cosX = dot( (p12-p11).ort(), ( p21-p22 ).ort() );
+        }
+
+      double Al = 0.5 * cross( p12 - X, p21 - X );
+      double Ar = 0.5 * cross( p22 - X, p11 - X );
+      vec2d Cl = (p12 + p21 + X) / 3.;
+      vec2d Cr = (p22 + p11 + X) / 3.;
+
+      vec2d r12 = vec3_to_vec2( src_to_dest * NORTH );
+      vec2d Fl = Al * siku.planet.R * K * r12.ort();
+      vec2d Fr = Ar * siku.planet.R * K * r12.ort();
+
+      vec2d F = Fl + Fr;
+
+      double torque1 = Kw * siku.planet.R_rec *
+          ( cross( Cl, Fl ) + cross( Cr, Fr ) );
+
+      double torque2 = Kw * siku.planet.R_rec *
+                ( cross( r12 - Cl, Fl ) + cross( r12 - Cr, Fr ) );
+
+      c.area = Ar > 0 ? Ar : 0.
+             + Al > 0 ? Al : 0.;
+
+      // signs are fitted manually
+      siku.es[c.i1].F -= vec2_to_vec3( F );
+      siku.es[c.i1].N -= torque1;
+
+      siku.es[c.i2].F += dest_to_src * vec2_to_vec3( F );
+      siku.es[c.i2].N += torque2;
+
+      double t = (abs(Al) + abs(Ar)) / ( siku.es[c.i1].A + siku.es[c.i2].A );
+      //c.durability -= (t > epsilon) ? t * sigma : 0.;
+    }
 }
